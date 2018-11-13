d434f1b6d97f26afa625035fc11d9d1563ec09c0
Poline Fedorova <polina.fedorova@devexpress.com>
2018-11-13 14:24:06 +0300

    upd vb

--- VB/MailMergeServer/Program.vb
+++ VB/MailMergeServer/Program.vb
@@ -3,10 +3,10 @@ Imports DevExpress.Snap
 Imports DevExpress.Snap.Core.API
 Imports DevExpress.XtraRichEdit
 Imports DevExpress.Snap.Core.Options
-Imports MailMergeServer.nwindDataSetTableAdapters
 Imports System
 Imports System.Data.OleDb
 Imports System.IO
+Imports nwindDataSetTableAdapters
 ' ...
 #End Region ' #Usings
 
@@ -40,7 +40,7 @@ Namespace MailMergeServer
             End If
             Console.WriteLine("Template file: {0}", (New FileInfo(templateFileName)).FullName)
             Console.WriteLine("Target file:   {0}", (New FileInfo(outputFileName)).FullName)
-'            #Region "#ServerCode"
+            '            #Region "#ServerCode"
             Dim server As New SnapDocumentServer()
 
             AddHandler server.SnapMailMergeRecordStarted, AddressOf server_SnapMailMergeRecordStarted
@@ -53,22 +53,22 @@ Namespace MailMergeServer
             options.DataSource = dataSource
             Console.Write("Performing mail merge... ")
             server.SnapMailMerge(options, outputFileName, DocumentFormat.Rtf)
-'            #End Region ' #ServerCode
+            '            #End Region ' #ServerCode
             Console.WriteLine("Ok!")
             Console.Write("Press any key...")
             Console.ReadKey()
             System.Diagnostics.Process.Start(outputFileName)
         End Sub
 
-        #Region "#RecordFinished"
+#Region "#RecordFinished"
         Private Shared Sub server_SnapMailMergeRecordFinished(ByVal sender As Object, ByVal e As SnapMailMergeRecordFinishedEventArgs)
             If e.RecordIndex = 3 Then
-            e.RecordDocument.AppendText("This is the third data record." & ControlChars.CrLf)
+                e.RecordDocument.AppendText("This is the third data record." & ControlChars.CrLf)
             End If
         End Sub
-        #End Region ' #RecordFinished
+#End Region ' #RecordFinished
 
-        #Region "#RecordStarted"
+#Region "#RecordStarted"
         Private Shared Sub server_SnapMailMergeRecordStarted(ByVal sender As Object, ByVal e As SnapMailMergeRecordStartedEventArgs)
             If e.RecordIndex = 3 Then
                 For i As Integer = 0 To e.RecordDocument.Fields.Count - 1
@@ -92,12 +92,12 @@ Namespace MailMergeServer
                 'e.RecordDocument.Fields[2].Update();
             End If
         End Sub
-        #End Region ' #RecordStarted
+#End Region ' #RecordStarted
 
         Private Shared Function CreateDataSource() As Object
             Dim dataSource = New nwindDataSet()
             Dim connection = New OleDbConnection()
-            connection.ConnectionString = My.Settings.Default.nwindConnectionString
+            connection.ConnectionString = My.Settings.nwindConnectionString
 
             Dim categories As New CategoriesTableAdapter()
             categories.Connection = connection
